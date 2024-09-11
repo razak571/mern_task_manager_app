77b0b125b782d147d41f16c8f0408878de3196ff
Razak Attar <165865038+razak571@users.noreply.github.com>
2024-09-11 11:22:51 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const domain =
-  process.env.NODE_ENV === "production" ? `.onrender.com` : "localhost";
+  process.env.NODE_ENV === "production" ? `.task-manager` : "localhost";
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
