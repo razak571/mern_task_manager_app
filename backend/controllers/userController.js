5e078bcafb71dd3cd5a7df3239b3170e7638ccca
Razak Attar <165865038+razak571@users.noreply.github.com>
2024-09-11 12:13:03 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,7 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const domain =
-  process.env.NODE_ENV === "production" ? `.taskmanger-server.onrender.com` : "localhost";
+  process.env.NODE_ENV === "production" ? "taskmanger-server.onrender.com" : "localhost";
 
 const signupUser = asyncHandler(async (req, res) => {
   const { firstname, lastname, email, password } = req.body;
